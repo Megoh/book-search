01e5e9ec03c9c97b063aa18f8800336ab1286338
unknown <dominikwolowicz@gmail.com>
2020-10-12 18:31:21 +0200

    Updates

---
+++ precache-manifest.d5c9dad07ad671cea405fba7c85e1c6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a5d72591f6eb0c57fb251791cb5554",
+    "url": "/book-search/index.html"
+  },
+  {
+    "revision": "8aee8b4614507baf7656",
+    "url": "/book-search/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "317a6e33ed56b54a2cd9",
+    "url": "/book-search/static/css/main.79214e0d.chunk.css"
+  },
+  {
+    "revision": "8aee8b4614507baf7656",
+    "url": "/book-search/static/js/2.a2b3f57b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/book-search/static/js/2.a2b3f57b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "317a6e33ed56b54a2cd9",
+    "url": "/book-search/static/js/main.ebbdff1f.chunk.js"
+  },
+  {
+    "revision": "a2766447fe38426fad70",
+    "url": "/book-search/static/js/runtime-main.53f04f00.js"
+  }
+]);
